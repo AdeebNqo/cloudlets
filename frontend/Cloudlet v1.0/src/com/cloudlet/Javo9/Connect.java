2be49cd44bf988c53bcdb6200761b91c40f94ebb
Jarvis Mutakha <mtkjar001@myuct.ac.za>
2014-07-24 20:28:16 +0200

    Filename change... oops!

---
+++ frontend/Cloudlet v1.0/src/com/cloudlet/Javo9/Connect.java
@@ -0,0 +1,17 @@
+package com.cloudlet.Javo9;
+
+//import com.example.cloudlet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Connect extends Activity 
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(com.example.cloudlet.R.layout.connect_screen);
+	}
+}
