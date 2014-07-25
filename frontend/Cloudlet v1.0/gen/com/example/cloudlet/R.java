f0a1c759240f1a50d43aca168c49f9587c472fcf
AdeebNqo <adeebnqo@gmail.com>
2014-07-25 18:16:23 +0200

    added untesed mqttt broker connection

--- frontend/Cloudlet v1.0/gen/com/example/cloudlet/R.java
+++ frontend/Cloudlet v1.0/gen/com/example/cloudlet/R.java
@@ -17,31 +17,47 @@ public final class R {
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int app_icon=0x7f020000;
-        public static final int male=0x7f020001;
+        public static final int icon=0x7f020001;
+        public static final int male=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int imageView1=0x7f080000;
-        public static final int textView1=0x7f080001;
+        public static final int action_settings=0x7f050009;
+        public static final int animation_layout=0x7f050004;
+        public static final int animation_layout_content=0x7f050001;
+        public static final int animation_layout_sidebar=0x7f050000;
+        public static final int content_button=0x7f050007;
+        public static final int imageView1=0x7f050002;
+        public static final int sidebar_button=0x7f050005;
+        public static final int sidebar_list=0x7f050006;
+        public static final int statustext=0x7f050003;
+        public static final int text=0x7f050008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int connect_screen=0x7f030001;
+        public static final int activity_service=0x7f030001;
+        public static final int connect_screen=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int connect_text=0x7f050003;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int connect_text=0x7f060003;
+        public static final int connectionfailed_text=0x7f060004;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_service=0x7f060005;
     }
     public static final class style {
         /** 
@@ -65,10 +81,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- frontend/Cloudlet v1.0/src/com/cloudlet/Javo9/Connect.java
+++ frontend/Cloudlet v1.0/src/com/cloudlet/Javo9/Connect.java
@@ -1,19 +1,88 @@
 package com.cloudlet.Javo9;
 
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import com.example.cloudlet.R;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Connect extends Activity 
 {
+	
+	//wifi state related vars
+	WifiManager manager = null;
+	WifiInfo info = null;
+	
+	//mqtt broker related vars
+	private String brokerIP = "10.10.0.2";
+	private int brokerPort = 9999;
+	private String brokerAddress = "tcp://"+brokerIP+":"+brokerPort;
+	private String deviceType = "client";
+	MemoryPersistence persistence = new MemoryPersistence();
+	MqttClient sampleClient = null ;
+	
+	//ui vars
+	TextView statustext;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.connect_screen);
+		statustext = (TextView) findViewById(R.id.statustext);
+		manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		info = manager.getConnectionInfo();
+		
+		new Connector().execute();
+	}
+	
+	//Class to connect to mqtt broker on the background
+	class Connector extends AsyncTask<Void, Void, Integer>{
+
+		@Override
+		protected Integer doInBackground(Void... params) {
+			try {
+				sampleClient = new MqttClient(brokerAddress, deviceType, persistence);
+				MqttConnectOptions connOpts = new MqttConnectOptions();
+	            connOpts.setCleanSession(true);
+	            sampleClient.subscribe("client/connect"); //client connection here
+	            sampleClient.subscribe("client/disconnect"); //client disconnects here
+	            sampleClient.subscribe("server/connecteduser"); //connected users will be broadcasted here
+	            sampleClient.subscribe("servers/service"); //requested service list will be broadcasted here
+	            
+	            //sending "connect MAC-address" string
+	            String macaddress = info.getMacAddress();
+	            String connectString = "connect "+macaddress;
+	            MqttMessage message = new MqttMessage(connectString.getBytes());
+	            sampleClient.publish("client/connect", message);
+	            return 0;
+			} catch (MqttException e) {
+				e.printStackTrace();
+				return 1;
+			}
+		}
+		
+	    @Override
+	    protected void onPostExecute(Integer connectStatus) {
+	    	if (connectStatus==0){
+	    		Intent intent = new Intent(Connect.this, ServiceActivity.class);
+	        	startActivity(intent);
+	    	}
+	    	else{
+	    		String connectionfail = getResources().getString(R.string.connectionfailed_text);
+	    		statustext.setText(connectionfail);
+	    	}
+	    }
 	}
 }
