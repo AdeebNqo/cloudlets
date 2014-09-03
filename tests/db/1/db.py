ffd4e3946d72d300eae7b0f0b400507d2e17a990
AdeebNqo <adeebnqo@gmail.com>
2014-09-03 17:16:34 +0200

    adding db tests

---
+++ tests/db/1/db.py
@@ -0,0 +1,20 @@
+#
+# Copyright 2014 Zola Mahlaza
+#
+class db(object):
+	def __init__(self,dbtype):
+		if (dbtype=='hamsterdb');
+			self.instance = hamster()
+		elif (dbtype=='mysql'):
+			self.instance = mysql()
+		elif (dbtype=='berkelydb'):
+			self.instance = berkelydb()
+class hamster(object):
+	def __init__(self):
+		a
+class mysql(object):
+	def __init__(self):
+		a
+class berkelydb(object):
+	def __init__(self):
+		a
