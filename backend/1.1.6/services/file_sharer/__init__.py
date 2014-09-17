4afa5d14c31632fc0cb14cb83d9a7411e3a7b53e
AdeebNqo <adeebnqo@gmail.com>
2014-09-17 10:58:05 +0200

    ignoring empty packets resulting from socket timeout

--- backend/1.1.6/services/file_sharer/__init__.py
+++ backend/1.1.6/services/file_sharer/__init__.py
@@ -107,80 +107,82 @@ class file_sharer():
 		data = ''
 		lastend = None
 		while True:
-			length = int(somesocket.recv(1024))
-			somesocket.sendall('OK')
-			if (lastend!=None):
-				data=data+lastend
-				lastend = None
-			data = data+somesocket.recv(length)
-			while (len(data) < length):
-				data = data + somesocket.recv(length)
-			if (len(data)>length):
-				lastend = data[length:]
-				data = data[:length]
-			packet = json.loads(data)
-			action = packet['action']
-			if action=='heartbeat':
-				response = '{\"status\":\"OK\"}'
-				somesocket.sendall(len(response))
-				statusresponse = somesocket.recv(1024)
-				if (statusresponse=='OK'):
-					somesocket.sendall(response)
-			elif action=='download':
-				print('downloading something')
-				owner = data['owner']
-				requester = data['requester']
-				filename = data['filename']
-				(idX, accessX, filenameX, ownerX, accesslistX) = self.currdb.get({'id':'{0}:{1}'.format(owner,filename)})
-				#check if request has access
-				if (accessX=='public' or requester==owner or requester in accesslistX):
-					jsonstring = "{\"actionresponse\":\"download\", \"OK\"}"
-					self.send(requester, jsonstring)
-				else:
-					jsonstring = "{\"actionresponse\":\"download\", \"status\":\"NOACCESS\"}"
-					self.send(requester, jsonstring)
-			elif action=='upload':
-				print('uploading something')
-				duration = data['duration']
-				access = data['access']
-				if (access!='public'):
-					accesslist = data['accesslist']
-				compression = data['compression']
-				filename = data['filename']
-				owner = data['owner']
-				objectdata = data['data']
-
-				if not os.path.exists(owner):
-					os.makedirs(owner)
-				filepath = '{0}/{1}'.format(owner, filename)
-				if not os.path.isfile(filepath):
-					_file = open(filepath, 'w')
-					_file.write(objectdata)
-					_file.close()
-					#primary key
-					primkey = '{0}:{1}'.format(owner,filename)
-					self.currdb.insert(('id', 'access', 'filename', 'owner', 'accesslist'), (primkey, access, filename, owner, accesslist.join(':')))
-			elif action=='transfer':
-				print('transefering something to someone')
-				owner = data['owner']
-				receiver = data['receiver']
-				oncloudlet = data['oncloudlet']
-				if (oncloudlet=='0'):
+			length = somesocket.recv(1024)
+			if (length!=''):
+				length = int(length)
+				somesocket.sendall('OK')
+				if (lastend!=None):
+					data=data+lastend
+					lastend = None
+				data = data+somesocket.recv(length)
+				while (len(data) < length):
+					data = data + somesocket.recv(length)
+				if (len(data)>length):
+					lastend = data[length:]
+					data = data[:length]
+				packet = json.loads(data)
+				action = packet['action']
+				if action=='heartbeat':
+					response = '{\"status\":\"OK\"}'
+					somesocket.sendall(len(response))
+					statusresponse = somesocket.recv(1024)
+					if (statusresponse=='OK'):
+						somesocket.sendall(response)
+				elif action=='download':
+					print('downloading something')
+					owner = data['owner']
+					requester = data['requester']
+					filename = data['filename']
+					(idX, accessX, filenameX, ownerX, accesslistX) = self.currdb.get({'id':'{0}:{1}'.format(owner,filename)})
+					#check if request has access
+					if (accessX=='public' or requester==owner or requester in accesslistX):
+						jsonstring = "{\"actionresponse\":\"download\", \"OK\"}"
+						self.send(requester, jsonstring)
+					else:
+						jsonstring = "{\"actionresponse\":\"download\", \"status\":\"NOACCESS\"}"
+						self.send(requester, jsonstring)
+				elif action=='upload':
+					print('uploading something')
+					duration = data['duration']
+					access = data['access']
+					if (access!='public'):
+						accesslist = data['accesslist']
+					compression = data['compression']
+					filename = data['filename']
+					owner = data['owner']
 					objectdata = data['data']
-				filename = data['filename']
 
-				(recv_socket, recv_address) = self.connections[receiver]
-				filepath = '{0}/{1}'.format(owner,filename)
-				if oncloudlet==1:
-					with open(filepath) as _file:
-						objectdata = _file.read()
-				jsonstring = '{\"action\":\"recv\", \"owner\":\"{0}\", \"filename\":\"{1}\", \"objectdata\":\"{2}\"}'.format(owner, filename, objectdata)
-				recv_socket.sendal(len(jsonstring))
-				if (recv_socket.recv(1024)=='OK'):
-					recv_socket.sendall(jsonstring)
-			elif action == 'identify':
-				data = json.loads(data)
-				self.users[data['username']] = (somesocket, address)
+					if not os.path.exists(owner):
+						os.makedirs(owner)
+					filepath = '{0}/{1}'.format(owner, filename)
+					if not os.path.isfile(filepath):
+						_file = open(filepath, 'w')
+						_file.write(objectdata)
+						_file.close()
+						#primary key
+						primkey = '{0}:{1}'.format(owner,filename)
+						self.currdb.insert(('id', 'access', 'filename', 'owner', 'accesslist'), (primkey, access, filename, owner, accesslist.join(':')))
+				elif action=='transfer':
+					print('transefering something to someone')
+					owner = data['owner']
+					receiver = data['receiver']
+					oncloudlet = data['oncloudlet']
+					if (oncloudlet=='0'):
+						objectdata = data['data']
+					filename = data['filename']
+
+					(recv_socket, recv_address) = self.connections[receiver]
+					filepath = '{0}/{1}'.format(owner,filename)
+					if oncloudlet==1:
+						with open(filepath) as _file:
+							objectdata = _file.read()
+					jsonstring = '{\"action\":\"recv\", \"owner\":\"{0}\", \"filename\":\"{1}\", \"objectdata\":\"{2}\"}'.format(owner, filename, objectdata)
+					recv_socket.sendal(len(jsonstring))
+					if (recv_socket.recv(1024)=='OK'):
+						recv_socket.sendall(jsonstring)
+				elif action == 'identify':
+					data = json.loads(data)
+					self.users[data['username']] = (somesocket, address)
 
 	def send(self,username, data):
 		(sock, addr) = self.users[username]
